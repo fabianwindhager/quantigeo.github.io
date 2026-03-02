2f672e21ae4e44898387484ec4618183fa8ed688
fabianwindhager <fabian.windhager@oeaw.ac.at>
2026-03-02 13:52:16 +0100

    update

--- Bevölkerungspyramide_Skript.R
+++ Bevölkerungspyramide_Skript.R
@@ -39,13 +39,23 @@ dta <- dta %>%
 # Gemtypen
 
 typ <- read.csv2("data/003_gliederungen_nach_städtischen_und_ländlichen_gebieten(1).csv")
+typ2 <- read.csv2("data/004_gliederungen_nach_städtischen_und_ländlichen_gebieten.csv")
+
 
 typ$gemnr <- as.character(typ$gemnr)
+typ2$gemnr <- as.character(typ2$gemnr)
+
 
 typ <- typ %>% 
   rename(gemname = Name,
          typ = Wert) %>% 
   select(-gemname)
+
+typ2 <- typ2 %>% 
+  select(-gemname)
+
+typ <- left_join(typ, typ2, by = "gemnr")
+
 
 
 ## Recoding typ$typ into typ$typ_rec
@@ -66,10 +76,23 @@ typ$typ <- typ$typ %>%
   )
 
 
+## Recoding typ$typ into typ$typ_rec
+typ$typ2 <- typ$typ2 %>%
+  as.character() %>%
+  fct_recode(
+    "Städte" = "1",
+    "Suburban" = "2",
+    "Ländliche Gebiete" = "3"
+  )
+
+
+
 dta <- left_join(dta, typ, by = "gemnr")
 dta$n <- as.numeric(dta$n)
 dta$alter <- as.factor(dta$alter)
 dta$typ <- as.factor(dta$typ)
+dta$typ2 <- as.factor(dta$typ2)
+
 
 glimpse(dta)
 write.csv(dta, "data/Bevölkerungspyramide.csv", row.names = FALSE)
@@ -94,10 +117,32 @@ ggplot(pyramid, aes(x=pop_pyramid, y=alter, fill=typ)) +
   labs(x="Bevölkerung",
        y="Altersgruppe",
        fill="",
-       title = "Bevölkerungspyramide Tirol nach Raumtyp 2002/2025")+
+       title = "Bevölkerungspyramide Tirol nach Raumtyp 2002/2025; \n Gliederung: Urban-Rural-Typologie der Statistik Austria")+
   facet_grid(.~jahr)+
   theme_bw()+
   theme(legend.position = "bottom")+
   theme(plot.title = element_text(hjust = 0.5))+
   annotate(geom="text", x=-15300, y=20, label="Männer", size=5) +
   annotate(geom="text", x=15300, y=20, label="Frauen", size=5) 
+
+
+ggplot(pyramid, aes(x=pop_pyramid, y=alter, fill=typ2)) + 
+  geom_col() +
+  scale_fill_manual(
+    values = c(
+      "Städte" = "#b35e6b",
+      "Suburban" = "#fdf453",
+      "Ländliche Gebiete" = "#84b870"))+
+  scale_x_continuous(labels=abs) +
+  geom_vline(xintercept=0, color="black") +
+  labs(x="Bevölkerung",
+       y="Altersgruppe",
+       fill="",
+       title = "Bevölkerungspyramide Tirol nach Raumtyp 2002/2025; \n Gliederung: Grad der Urbanisierung der Europäischen Kommission")+
+  facet_grid(.~jahr)+
+  theme_bw()+
+  theme(legend.position = "bottom")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  annotate(geom="text", x=-15300, y=20, label="Männer", size=5) +
+  annotate(geom="text", x=15300, y=20, label="Frauen", size=5) 
+
